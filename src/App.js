9f36d40608b797efeb8306e1baad595ea38eebfa
Yilfri Salave <yilfri97@hotmail.com>
2021-05-17 00:45:08 -0400

    Fomulario added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import Header from './components/Header';
+import Formulario from './components/Formulario';
+
+import styled from '@emotion/styled';
+
+const Contenedor = styled.div`
+	max-width: 600px;
+	margin: 0 auto;
+`;
+
+const ContenedorFomulario = styled.div`
+	background-color: #fff;
+	padding: 3rem;
+`;
 
 function App() {
-	return <Header titulo="Cotizador de Vehículos " />;
+	return (
+		<Contenedor>
+			<Header titulo="Cotizador de Vehículos " />
+
+			<ContenedorFomulario>
+				<Formulario />
+			</ContenedorFomulario>
+		</Contenedor>
+	);
 }
 
 export default App;

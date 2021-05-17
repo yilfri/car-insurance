f1d3466132e4da7fb22da6fbb9f5cff2013df5bb
Yilfri Salave <yilfri97@hotmail.com>
2021-05-17 00:26:17 -0400

    Styled-Components added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const ContenedorHeader = styled.header`
+	background-color: #26c6da;
+	padding: 10px;
+	font-weight: bold;
+	color: #ffffff;
+`;
+
+const TextoHeader = styled.h1`
+	font-size: 2rem;
+	margin: 0;
+	font-family: 'Slabo 27px', serif;
+	text-align: center;
+`;
 
 const Header = ({ titulo }) => (
-	<header>
-		<h1>{titulo}</h1>
-	</header>
+	<ContenedorHeader>
+		<TextoHeader>{titulo}</TextoHeader>
+	</ContenedorHeader>
 );
 
 export default Header;

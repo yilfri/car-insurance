bcbdfe960a9b238810787340d091e0f9bcbbea2c
Yilfri Salave <yilfri97@hotmail.com>
2021-05-18 02:50:01 -0400

    helper added

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styled from '@emotion/styled';
+import { obtenerDiferenciaYear, obtenerMarca } from '../helper';
 
 const Campo = styled.div`
 	display: flex;
@@ -41,27 +42,65 @@ const Boton = styled.button`
 	}
 `;
 
+const Error = styled.div`
+	background-color: red;
+	padding: 1rem;
+	color: white;
+	width: 100%;
+	text-align: center;
+	margin-bottom: 2rem;
+`;
+
 const Formulario = () => {
+	// State para los datos.
 	const [datos, setDatos] = useState({
 		marca: '',
 		year: '',
 		plan: 'basico'
 	});
+	// State para el manejo del Error.
+	const [error, setError] = useState(false);
 
+	// Destructuración del State.
 	const { marca, year, plan } = datos;
 
-	const obtenerInformacion = (e) => {
+	// Manejador del evento onChange.
+	const handleChange = (e) => {
 		setDatos({
 			...datos,
 			[e.target.name]: e.target.value
 		});
 	};
 
+	// Manejador del evento onSubmit.
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		if (marca.trim() === '' || year.trim() === '' || plan.trim() === '') {
+			setError(true);
+			return;
+		}
+		setError(false);
+
+		// Base de 2000.
+		let resultado = 2000;
+
+		// Obtener diferencia de año
+		const diferencia = obtenerDiferenciaYear(year);
+
+		// Resta 3% por cada año.
+		resultado -= (diferencia * 3 * resultado) / 100;
+
+		resultado = obtenerMarca(marca) * resultado;
+		console.log(resultado);
+	};
+
 	return (
-		<form>
+		<form onSubmit={handleSubmit}>
+			{error ? <Error>Todos los campos son obligatorios</Error> : null}
 			<Campo>
 				<Label>Marca</Label>
-				<Select name="marca" value={marca} onChange={obtenerInformacion}>
+				<Select name="marca" value={marca} onChange={handleChange}>
 					<option value="">-- Seleccione --</option>
 					<option value="americano">Americano</option>
 					<option value="europeo">Europeo</option>
@@ -70,8 +109,10 @@ const Formulario = () => {
 			</Campo>
 			<Campo>
 				<Label>Año</Label>
-				<Select name="year" value={year} onChange={obtenerInformacion}>
+				<Select name="year" value={year} onChange={handleChange}>
 					<option value="">-- Seleccione --</option>
+					<option value="2023">2023</option>
+					<option value="2022">2022</option>
 					<option value="2021">2021</option>
 					<option value="2020">2020</option>
 					<option value="2019">2019</option>
@@ -91,7 +132,7 @@ const Formulario = () => {
 					name="plan"
 					value="basico"
 					checked={plan === 'basico'}
-					onChange={obtenerInformacion}
+					onChange={handleChange}
 				/>
 				Básico
 				<InputRadio
@@ -99,11 +140,11 @@ const Formulario = () => {
 					name="plan"
 					value="completo"
 					checked={plan === 'completo'}
-					onChange={obtenerInformacion}
+					onChange={handleChange}
 				/>
 				Completo
 			</Campo>
-			<Boton type="button">Cotizar</Boton>
+			<Boton type="submit">Cotizar</Boton>
 		</form>
 	);
 };
